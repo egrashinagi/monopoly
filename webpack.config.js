8d821880162482ac6ccb01365140b878319fc52d
Egrashina Galina <egrashina.gi@gmail.com>
2017-12-28 19:24:21 +0300

    Мелкие исправления и настройка webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -21,13 +22,28 @@ module.exports = {
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
         })
+      },
+      {
+        test:/\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              publicPath: 'dist/assets/',
+              outputPath: 'src/images/'
+            }
+          }
+        ]
       }
     ],
     loaders: [
-      { test: /\.html$/, loader: "html-loader" }
+      { test: /\.html$/, loader: "html-loader" },
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin('styles.css'),
     new HtmlWebpackPlugin({
       title: 'Custom template',
       template: './src/index.html',
